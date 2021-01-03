189e19217224c47ec5bf2ac9be2b55e7abcf9bfb
Viral Sangani <virals@geekyants.com>
2021-01-03 11:01:12 +0530

    Server side props added

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function Index({ allPosts }) {
   );
 }
 
-export async function getStaticProps({ preview }) {
+export async function getServerSideProps({ preview }) {
   const allPosts = (await getAllPostsForHome(preview)) || [];
   return {
     props: { allPosts },
